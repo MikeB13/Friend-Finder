3492ed2d72e1b6ed23b2e5f770259f6879476555
Mike <mikhailbabayev@Mikhails-MacBook-Pro.local>
2019-07-09 22:19:33 -0400

    completed files

---
+++ App/data/friends.js
@@ -0,0 +1,102 @@
+// DATA
+
+var friends = [{
+        name: "Ahmed",
+        photo: "https://placebeard.it/640x360",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "John Smith",
+        photo: "https://www.stevensegallery.com/640/360",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Jane Doe",
+        photo: "https://www.stevensegallery.com/640/360",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Abe Lincoln",
+        photo: "https://www.stevensegallery.com/640/360",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Average Joe",
+        photo: "https://www.stevensegallery.com/640/360",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Pacman",
+        photo: "https://www.stevensegallery.com/640/360",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
+];
+
+module.exports = friends;
---
+++ App/routing/apiRoutes.js
@@ -0,0 +1,60 @@
+// LOAD DATA
+
+var friends = require("../data/friends");
+
+
+// ROUTES
+
+module.exports = function(app) {
+    // API GET Requests
+
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+
+    // API POST Requests
+
+
+    app.post("/api/friends", function(req, res) {
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
+
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        var totalDifference;
+
+        // loop through all the friend possibilities in the database.
+        for (var i = 0; i < friends.length; i++) {
+            var currentFriend = friends[i];
+            totalDifference = 0;
+
+            console.log(currentFriend.name);
+
+            // then loop through all the scores of each friend
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
+
+                // calculate the difference between the scores and sum them into the totalDifference
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            }
+
+            if (totalDifference <= bestMatch.friendDifference) {
+                // Reset 
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+        }
+
+        friends.push(userData);
+
+        res.json(bestMatch);
+    });
+};
